65d9224308c993f7688e5615f705fe31eacd1384
Adarshlpu <adarshkumar709121@gmail.com>
2024-10-03 04:13:11 +0530

    setup of axios instance done

--- Lms/lms-frontend/src/main.jsx
+++ Lms/lms-frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './Redux/store';
 
 
 createRoot(document.getElementById('root')).render(
