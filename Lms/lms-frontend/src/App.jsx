cdd2741ed8f0252352020f742c4ac6854117aad3
Adarshlpu <adarshkumar709121@gmail.com>
2024-10-04 01:01:12 +0530

    updated footer and home page layout

--- Lms/lms-frontend/src/App.jsx
+++ Lms/lms-frontend/src/App.jsx
@@ -1,17 +1,60 @@
-import './App.css'
+// import './App.css';
 
 // import { Routes } from 'react-router-dom';
+
+// import Footer from './Components/Footer';
+
+// import HomeLayout from './Layouts/HomeLayout'; 
+// function App() {
+
+
+//   return (
+//     <>
+//     {/* <Footer /> */}
+//     <HomeLayout />
+//     </>
+  
+//     )
+// };
+
+// export default App
+
+// import './App.css';
+// import Footer from './Components/Footer';
+// import HomeLayout from './Layouts/HomeLayout';  // Import HomeLayout component
+
+// function App() {
+//   return (
+//     <>
+//       {/* Uncomment this line if you want to include Footer */}
+//       {/* <Footer /> */}
+//       <HomeLayout />  {/* Make sure HomeLayout is imported correctly */}
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+//3rd chatgpt
+
+
+
+import './App.css';
 import Footer from './Components/Footer';
+import HomeLayout from './Layouts/HomeLayout';
+
 function App() {
-
-
   return (
     <>
-   
-   <Footer />
-
+      {/* Uncomment if you want to use the Footer */}
+      <Footer />
+      <HomeLayout />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
